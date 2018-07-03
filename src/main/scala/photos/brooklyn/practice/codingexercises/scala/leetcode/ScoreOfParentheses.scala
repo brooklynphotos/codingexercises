b9c0feadf0a3837a142f66c62766640aa725c1a6
Brooklyn Photos <guoneng.list@gmail.com>
2018-07-03 11:04:32 -0400

    added tailrec annotation

--- src/main/scala/photos/brooklyn/practice/codingexercises/scala/leetcode/ScoreOfParentheses.scala
+++ src/main/scala/photos/brooklyn/practice/codingexercises/scala/leetcode/ScoreOfParentheses.scala
@@ -1,5 +1,7 @@
 package photos.brooklyn.practice.codingexercises.scala.leetcode
 
+import scala.annotation.tailrec
+
 /**
   * https://leetcode.com/contest/weekly-contest-90/problems/score-of-parentheses/
   */
@@ -12,11 +14,12 @@ object ScoreOfParentheses {
     * 2. another layer within () to figure out what the score is for ()
     * Similarly, we have one bottom layer for ()(), and then another layer is created and then destroyed to figure out
     * (), and another one for the second ()
-    * @param str
-    * @param pointer
-    * @param stack
+    * @param str original string
+    * @param pointer character position to consider
+    * @param stack keeps track of which layer of the () we are in
     * @return
     */
+  @tailrec
   def walkParentheses(str: String, pointer: Int=0, stack: List[Int]=List(0)): Int =
     if(str.length==pointer)
       stack.head
@@ -28,5 +31,3 @@ object ScoreOfParentheses {
 
   def getScore(layer: Int) = if(layer==0) 1 else layer * 2
 }
-
-case class Group(val subgroups: List[Group])
