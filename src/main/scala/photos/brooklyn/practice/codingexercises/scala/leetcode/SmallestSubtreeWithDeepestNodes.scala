43a6a2ce7f7925b51a910c9b10e59fc0e4070cea
Brooklyn Photos <guoneng.list@gmail.com>
2018-07-08 22:57:28 -0400

    Week 92 problem 3 more streamlined

--- src/main/scala/photos/brooklyn/practice/codingexercises/scala/leetcode/SmallestSubtreeWithDeepestNodes.scala
+++ src/main/scala/photos/brooklyn/practice/codingexercises/scala/leetcode/SmallestSubtreeWithDeepestNodes.scala
@@ -7,19 +7,15 @@ import photos.brooklyn.practice.codingexercises.scala.leetcode.model.TreeNode
   */
 object SmallestSubtreeWithDeepestNodes {
 
-  def increment(res: (TreeNode, Int, Int)): (TreeNode, Int, Int) = (res._1, res._2+1, res._3+1)
+  def increment(res: (TreeNode, Int)): (TreeNode, Int) = (res._1, res._2+1)
 
-  def findSubtree(node: TreeNode, depth: Int=0): (TreeNode, Int, Int) = (node.left, node.right) match{
-    case (null, null) => (node, 0, 0) // if node is a leaf, it is its own answer
-    case (left, null) => increment(findSubtree(left))
-    case (null, right) => increment(findSubtree(right))
-    case (left, right) => (findSubtree(left), findSubtree(right)) match {
+  def findSubtree(node: TreeNode): (TreeNode, Int) = if(node==null) (node, -1) else
+    (findSubtree(node.left), findSubtree(node.right)) match {
       case (leftRes, rightRes) =>
-        if(leftRes._3==rightRes._3) (node, 0, leftRes._3+1)
-        else if(leftRes._3>rightRes._3) increment(leftRes)
+        if(leftRes._2==rightRes._2) (node, leftRes._2+1)
+        else if(leftRes._2>rightRes._2) increment(leftRes)
         else increment(rightRes)
     }
-  }
 
   def subtreeWithAllDeepest(root: TreeNode): TreeNode = findSubtree(root)._1
 }
