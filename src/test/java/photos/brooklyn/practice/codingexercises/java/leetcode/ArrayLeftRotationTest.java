b64ecc0f1a856dd9972fc11ab25af8bf626f157b
Brooklyn Photos <guoneng.list@gmail.com>
2018-07-25 19:58:59 -0400

    hackerrank problem https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem

---
+++ src/test/java/photos/brooklyn/practice/codingexercises/java/leetcode/ArrayLeftRotationTest.java
@@ -0,0 +1,13 @@
+package photos.brooklyn.practice.codingexercises.java.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayLeftRotationTest {
+
+    @Test
+    public void rotLeft() {
+        assertArrayEquals(new int[]{5,1,2,3,4}, ArrayLeftRotation.rotLeft(new int[]{1,2,3,4,5}, 4));
+    }
+}
