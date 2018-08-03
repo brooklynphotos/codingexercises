e37ae2e8375a53622ea25c23eaea532272095847
Brooklyn Photos <guoneng.list@gmail.com>
2018-08-02 22:45:46 -0400

    Week 95 problem 4 probably works but times out

--- src/main/java/photos/brooklyn/practice/codingexercises/java/leetcode/ProfitableSchemes.java
+++ src/main/java/photos/brooklyn/practice/codingexercises/java/leetcode/ProfitableSchemes.java
@@ -1,30 +1,22 @@
 package photos.brooklyn.practice.codingexercises.java.leetcode;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * https://leetcode.com/contest/weekly-contest-95/problems/profitable-schemes/
  */
 public class ProfitableSchemes {
     public int profitableSchemes(int G, int P, int[] group, int[] profit) {
-        if (P <= 0) {
-            return 1 + combin(group.length) - 1;
-        }
-        if(G<=0) return 0;
-        final Set<Integer> taken = new HashSet<>();
+        if(G<0) return 0;
         int count = 0;
         for (int i = 0; i < group.length; i++) {
             final int g = group[i];
             final int p = profit[i];
-            taken.add(i);
-            count += profitableSchemes(G - g, P - p, Arrays.copyOfRange(group, i + 1, group.length), Arrays.copyOfRange(profit, i + 1, profit.length));
+            final int remainingP = P - p;
+            final int remainingG = G - g;
+            if(remainingG<0) continue;
+            count += (remainingP<=0 ? 1 : 0) + profitableSchemes(remainingG, remainingP, Arrays.copyOfRange(group, i + 1, group.length), Arrays.copyOfRange(profit, i + 1, profit.length));
         }
         return count;
-    }
-
-    private static int combin(final int n) {
-        return 1 << n;
     }
 }
--- src/test/java/photos/brooklyn/practice/codingexercises/java/leetcode/ProfitableSchemesTest.java
+++ src/test/java/photos/brooklyn/practice/codingexercises/java/leetcode/ProfitableSchemesTest.java
@@ -24,4 +24,31 @@ public class ProfitableSchemesTest {
         int[] profit = {6, 7, 8};
         assertEquals(7, obj.profitableSchemes(G, P, group, profit));
     }
+
+    @Test
+    public void profitableSchemes_short_impossible() {
+        int G = 1;
+        int P = 1;
+        int[] group = {2, 3, 5};
+        int[] profit = {6, 7, 8};
+        assertEquals(0, obj.profitableSchemes(G, P, group, profit));
+    }
+
+    @Test
+    public void profitableSchemes_tiny() {
+        int G = 1;
+        int P = 1;
+        int[] group = {1};
+        int[] profit = {1};
+        assertEquals(1, obj.profitableSchemes(G, P, group, profit));
+    }
+
+    @Test
+    public void profitableSchemes_tiny2() {
+        int G = 1;
+        int P = 1;
+        int[] group = {1,1,1,1,2,2,1,2,1,1};
+        int[] profit = {0,1,0,0,1,1,1,0,2,2};
+        assertEquals(4, obj.profitableSchemes(G, P, group, profit));
+    }
 }
