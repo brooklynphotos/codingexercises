f43e489923980514bfaa313419633be9fbce9222
Brooklyn Photos <guoneng.list@gmail.com>
2018-08-04 23:05:17 -0400

    Week 96 problem 2 fails a test. Likely wrong approach

---
+++ src/main/java/photos/brooklyn/practice/codingexercises/java/leetcode/BoatsToSavePeople.java
@@ -0,0 +1,42 @@
+package photos.brooklyn.practice.codingexercises.java.leetcode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class BoatsToSavePeople {
+    public int numRescueBoats(int[] people, int limit) {
+        final Set<Integer> peopleWaiting = IntStream.range(0, people.length).boxed().collect(Collectors.toSet());
+        final int halfLimit = limit / 2;
+        final int[] weightList = Arrays.stream(people)
+                .boxed()
+                .sorted(Comparator.comparingInt(a -> Math.abs(a - halfLimit)))
+                .mapToInt(Integer::intValue)
+                .toArray();
+        int boatCount = 0;
+        for (int i=0;i<weightList.length;i++) {
+            // no longer waiting
+            if(!peopleWaiting.contains(i)) continue;
+            boardBoat(i, peopleWaiting, weightList, limit);
+            boatCount++;
+        }
+        return boatCount;
+    }
+
+    private void boardBoat(final Integer personIndex, final Set<Integer> peopleWaiting, final int[] weightList, final int limit) {
+        peopleWaiting.remove(personIndex);
+        // find a possible partner
+        final int spaceLeft = limit - weightList[personIndex];
+        for (int i = personIndex + 1; i < weightList.length && spaceLeft > 0; i++) {
+            final int weight = weightList[i];
+            // found his buddy
+            if (weight <= spaceLeft) {
+                peopleWaiting.remove(i);
+                break;
+            }
+        }
+    }
+}
---
+++ src/test/java/photos/brooklyn/practice/codingexercises/java/leetcode/BoatsToSavePeopleTest.java
@@ -0,0 +1,51 @@
+package photos.brooklyn.practice.codingexercises.java.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BoatsToSavePeopleTest {
+    private BoatsToSavePeople obj = new BoatsToSavePeople();
+
+    @Test
+    public void numRescueBoats_small() {
+        final int[] people = {1,2};
+        final int limit = 3;
+        assertEquals(1, obj.numRescueBoats(people, limit));
+    }
+
+    @Test
+    public void numRescueBoats_small3_3() {
+        final int[] people = {3,2,2,1};
+        final int limit = 3;
+        assertEquals(3, obj.numRescueBoats(people, limit));
+    }
+
+    @Test
+    public void numRescueBoats_small4() {
+        final int[] people = {3,5,3,4};
+        final int limit = 5;
+        assertEquals(4, obj.numRescueBoats(people, limit));
+    }
+
+    @Test
+    public void numRescueBoats_small3_6() {
+        final int[] people = {3,2,3,2,2};
+        final int limit = 6;
+        assertEquals(3, obj.numRescueBoats(people, limit));
+    }
+
+    @Test
+    public void numRescueBoats_small7_4() {
+        final int[] people = {5,1,7,4,2,4};
+        final int limit = 7;
+        assertEquals(4, obj.numRescueBoats(people, limit));
+    }
+
+    @Test
+    public void numRescueBoats_larger1000_5() {
+        final int[] people = {445,597,385,576,291,190,187,613,657,477};
+        final int limit = 1000;
+        assertEquals(5, obj.numRescueBoats(people, limit));
+    }
+}
