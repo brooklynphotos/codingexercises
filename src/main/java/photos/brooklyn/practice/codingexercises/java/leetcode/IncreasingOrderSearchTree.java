c841b025f87fb71f3f022354d8452544401aa960
Brooklyn Photos <guoneng.list@gmail.com>
2018-09-01 22:56:21 -0400

    Week 100 problem 2

---
+++ src/main/java/photos/brooklyn/practice/codingexercises/java/leetcode/IncreasingOrderSearchTree.java
@@ -0,0 +1,39 @@
+package photos.brooklyn.practice.codingexercises.java.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/contest/weekly-contest-100/problems/increasing-order-search-tree/
+ */
+public class IncreasingOrderSearchTree {
+    public TreeNode increasingBST(TreeNode root) {
+        final List<Integer> list = toList(root);
+        return toRightTree(list.toArray(new Integer[0]), 0);
+    }
+
+    private static TreeNode toRightTree(final Integer[] list, int index) {
+        if(index==list.length) return null;
+        final TreeNode n = new TreeNode(list[index]);
+        n.right = toRightTree(list, index + 1);
+        return n;
+    }
+
+    private static List<Integer> toList(final TreeNode root) {
+        final List<Integer> l = root.left==null ? new LinkedList<>() : toList(root.left);
+        l.add(root.val);
+        if(root.right != null)
+            l.addAll(toList(root.right));
+        return l;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
