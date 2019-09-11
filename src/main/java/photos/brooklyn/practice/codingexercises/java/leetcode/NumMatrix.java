ddfcdbc58e5b768875f190a3298bd846a9e51562
Brooklyn Photos <guoneng.list@gmail.com>
2019-09-11 23:37:39 +0200

    successfully passed tests

--- src/main/java/photos/brooklyn/practice/codingexercises/java/leetcode/NumMatrix.java
+++ src/main/java/photos/brooklyn/practice/codingexercises/java/leetcode/NumMatrix.java
@@ -10,11 +10,12 @@ public class NumMatrix {
     }
 
     static int[][] makeSumMatrix(int[][] matrix) {
+        if(matrix.length==0 || matrix[0].length==0) return new int[1][1];
         final int[][] sumMatrix = new int[matrix.length + 1][matrix[0].length + 1];
         for (int row = 0; row < sumMatrix.length; row++) {
             for (int col = 0; col < sumMatrix[row].length; col++) {
                 if (row > 0 && col > 0) {
-                    sumMatrix[row][col] = sumMatrix[row][col - 1] + (row>1 ? matrix[row - 2][col - 1] : 0) + matrix[row - 1][col - 1];
+                    sumMatrix[row][col] = sumMatrix[row][col - 1] + sumMatrix[row - 1][col] - sumMatrix[row - 1][col - 1] + matrix[row - 1][col - 1];
                 }
             }
         }
@@ -22,7 +23,8 @@ public class NumMatrix {
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return 1;
+        // the parts to exclude and the corner to re-include are outside the boundaries, therefore they don't have 1 added
+        return sumMatrix[row2 + 1][col2 + 1] - sumMatrix[row1][col2 + 1] - sumMatrix[row2 + 1][col1] + sumMatrix[row1][col1];
     }
 
     public static void main(String[] args) {
--- src/test/java/photos/brooklyn/practice/codingexercises/java/leetcode/NumMatrixTest.java
+++ src/test/java/photos/brooklyn/practice/codingexercises/java/leetcode/NumMatrixTest.java
@@ -17,4 +17,36 @@ public class NumMatrixTest {
         assertEquals(12, res[2][2]);
         assertEquals(21, res[2][3]);
     }
+    @Test
+    public void makeSumMatrixLarger() {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {-1, -2, -3},
+            {-4, -5, -6}
+        };
+        int[][] res = NumMatrix.makeSumMatrix(matrix);
+        assertEquals(0, res[4][3]);
+        assertEquals(9, res[3][2]);
+    }
+
+    @Test
+    public void sumRegion() {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {-1, -2, -3},
+            {-4, -5, -6}
+        };
+        final NumMatrix m = new NumMatrix(matrix);
+        assertEquals(0, m.sumRegion(0,0,3,2));
+        assertEquals(6, m.sumRegion(1,1,2,2));
+        assertEquals(5, m.sumRegion(1,1,1,1));
+    }
+    @Test
+    public void sumRegionEmpty() {
+        int[][] matrix = {{}};
+        final NumMatrix m = new NumMatrix(matrix);
+        assertEquals(0, m.sumRegion(0,0,0,0));
+    }
 }
