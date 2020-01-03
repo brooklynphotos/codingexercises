1d6f5fc55276aa0957c1a4f1f8eceff6bc1d833d
Brooklyn Photos <guoneng.list@gmail.com>
2020-01-03 22:29:15 +0100

    passed all unit tests

---
+++ src/main/java/photos/brooklyn/practice/codingexercises/java/codility/MinMaxDivision.java
@@ -0,0 +1,56 @@
+package photos.brooklyn.practice.codingexercises.java.codility;
+
+public class MinMaxDivision {
+    /**
+     *
+     * @param K number of blocks to divide into, each block is between 0 and N, incl
+     * @param M not used, it is the upperbound of elements in A, it is between 0 and 10,000 incl
+     * @param A each element is between 0 and M, incl, size is N
+     * @return of all the K-block combinations, the combination with smallest "large sum", return that sum
+     */
+    public int solution(int K, int M, int[] A) {
+        final long[] sums = new long[A.length];
+        sums[0] = A[0];
+        for (int i = 1; i < A.length; i++) {
+            sums[i] = sums[i - 1] + A[i];
+        }
+        // sums is now a sorted array
+        // loop through K to divide up the array
+        final long ASum = sums[A.length - 1];
+        long previousSum = 0;
+        int currentPointer = -1;
+        long largeSum = 0;
+        for (int k = 0; k < K; k++) {
+            long currentSum = ASum - previousSum;
+            // this is the minimum sum we can achieve
+            final long minSum = currentSum / (K - k);
+            // search for a sums index whose value is closest to this minSum without going over
+            // search needs to be for minSum plus previousSum since all the numbers now include that previous sum
+            currentPointer = search(sums, minSum + previousSum, currentPointer + 1);
+            long newSum = sums[currentPointer];
+            largeSum = Math.max(newSum - previousSum, largeSum);
+            previousSum = newSum;
+        }
+        return (int)largeSum;
+    }
+
+    static int search(long[] arr, long ceiling, int start) {
+        int end = arr.length - 1;
+        while (start < end) {
+            int divider = (start + end) / 2;
+            long dividerV = arr[divider];
+            if (dividerV == ceiling || (divider + 1 < arr.length && arr[divider] < ceiling && arr[divider + 1] > ceiling)) {
+                return divider;
+            }
+            if (divider + 1 < arr.length && arr[divider + 1] == ceiling) {
+                return divider + 1;
+            }
+            if (ceiling > dividerV) {
+                start = divider;
+            } else {
+                end = divider;
+            }
+        }
+        return end;
+    }
+}
---
+++ src/test/scala/photos/brooklyn/practice/codingexercises/java/codility/MinMaxDivisionTest.java
@@ -0,0 +1,63 @@
+package photos.brooklyn.practice.codingexercises.java.codility;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MinMaxDivisionTest {
+
+    @Test
+    public void givenSortedListAndCeiling_returnCorrectIndex() {
+        final long[] list = {1l, 2l, 3l, 4l, 6l};
+        assertEquals(3, MinMaxDivision.search(list, 4, 0));
+        assertEquals(3, MinMaxDivision.search(list, 5, 0));
+        assertEquals(3, MinMaxDivision.search(list, 4, 1));
+    }
+
+    @Test
+    public void givenSample_returnCorrect() {
+        int K = 3;
+        int M = 5;
+        int[] A = {2, 1, 5, 1, 2, 2, 2};
+        assertEquals(6, new MinMaxDivision().solution(K, M, A));
+    }
+    @Test
+    public void givenOneBlock_returnSum() {
+        int K = 1;
+        int M = 5;
+        int[] A = {2, 1, 5, 1, 2, 2, 2};
+        assertEquals(15, new MinMaxDivision().solution(K, M, A));
+    }
+    @Test
+    public void givenAllZerosButOne_returnThatONe() {
+        int K = 5;
+        int M = 15;
+        int[] A = {0, 0, 0, 15, 0, 0, 0, 0};
+        assertEquals(15, new MinMaxDivision().solution(K, M, A));
+    }
+    @Test
+    public void givenExtraneousBlocksAndSingleA_returnSingleElement() {
+        int K = 100;
+        int M = 5;
+        int[] A = {5};
+        assertEquals(5, new MinMaxDivision().solution(K, M, A));
+    }
+    @Test
+    public void givenExtraneousBlocks_returnMaxA() {
+        int K = 100;
+        int M = 5;
+        int[] A = {2,3,1};
+        assertEquals(3, new MinMaxDivision().solution(K, M, A));
+    }
+    @Test
+    public void givenLargestUniformSample_returnUnitSize() {
+        int K = 100_000;
+        int M = 10_000;
+        int[] A = new int[100_000];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = M;
+        }
+        assertEquals(M, new MinMaxDivision().solution(K, M, A));
+    }
+}
