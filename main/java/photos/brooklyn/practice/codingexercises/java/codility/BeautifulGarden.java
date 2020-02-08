85d512ef40e670b545cc87574d69b8ce2d172777
Brooklyn Photos <guoneng.list@gmail.com>
2020-02-08 23:36:19 +0100

    not quite working beaitiful garden

--- main/java/photos/brooklyn/practice/codingexercises/java/codility/BeautifulGarden.java
+++ main/java/photos/brooklyn/practice/codingexercises/java/codility/BeautifulGarden.java
@@ -8,6 +8,23 @@ public class BeautifulGarden {
      * @return
      */
     public int solution(int[] A) {
-        throw new UnsupportedOperationException();
+        return findMinCut(A, 0);
+    }
+
+    /**
+     * Finds the minimum number of cuts
+     * @param treeHeights
+     * @param pivotIndex
+     * @return
+     */
+    private int findMinCut(final int[] treeHeights, final int pivotIndex) {
+        int min = Integer.MAX_VALUE;
+        // decending all
+        if (treeHeights[pivotIndex] > treeHeights[pivotIndex + 1] && treeHeights[pivotIndex + 1] > treeHeights[pivotIndex + 2]) {
+            // cut first
+            return Math.min(1 + findMinCut(treeHeights, pivotIndex + 1), 1 + findMinCut(treeHeights, pivotIndex + 2));
+        }
+        return findMinCut(treeHeights, pivotIndex + 1);
+
     }
 }
