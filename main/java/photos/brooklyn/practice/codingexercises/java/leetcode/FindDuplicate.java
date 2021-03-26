0ae783f7e1a3b17437f8fca7c254467b1f0983a0
Guoneng Zhong <guoneng.list@gmail.com>
2021-03-26 08:42:03 +0100

    other ways, slightly faster

--- main/java/photos/brooklyn/practice/codingexercises/java/leetcode/FindDuplicate.java
+++ main/java/photos/brooklyn/practice/codingexercises/java/leetcode/FindDuplicate.java
@@ -7,6 +7,32 @@ import java.util.Arrays;
  */
 public class FindDuplicate {
     public int findDuplicate(int[] nums) {
+        return substitution(nums);
+    }
+
+    private static int extraArray(int[] nums) {
+        // put numbers in this new map until you find one that is already occupied
+        final int[] map = new int[nums.length - 1];
+        for (final int n : nums) {
+            final int index = n - 1;
+            if (map[index] > 0) return n;
+            map[index] = n;
+        }
+        throw new IllegalArgumentException("Array has all unique numbers");
+    }
+
+    private static int substitution(int[] nums) {
+        int n = nums[0];
+        while (n < nums.length) {
+            if (n == nums[n]) return n;
+            int x = nums[n];
+            nums[n] = n;
+            n = x;
+        }
+        throw new IllegalArgumentException("Array has all unique numbers");
+    }
+
+    private static int useSort(int[] nums) {
         Arrays.sort(nums);
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] == nums[i - 1]) {
